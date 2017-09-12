0931032241dfa1fbb7042a168b3b8a9178e1d1af
Darnell Gillead <dlgillead@gmail.com>
2017-09-12 15:32:34 -0500

    created FindFactorial method

---
+++ Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number you would like to find the factorial of: ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            int factorial = FindFactorial(num);
+
+            Console.WriteLine("The factorial of {0} is {1}.", num, factorial);
+        }
+
+        private static int FindFactorial(int num)
+        {
+            int returnFactorial;
+
+            returnFactorial = 4;
+
+            return returnFactorial;
+        }
+    }
+}
